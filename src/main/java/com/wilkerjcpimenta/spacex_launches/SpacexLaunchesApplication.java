5007104b41f1761f3531d8f1279ae5b5b439f64d
WilJCPimenta <wiljcpimenta@gmail.com>
2025-02-13 15:30:30 -0300

    First Commit

---
+++ src/main/java/com/wilkerjcpimenta/spacex_launches/SpacexLaunchesApplication.java
@@ -0,0 +1,13 @@
+package com.wilkerjcpimenta.spacex_launches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacexLaunchesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacexLaunchesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wilkerjcpimenta/spacex_launches/SpacexLaunchesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilkerjcpimenta.spacex_launches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacexLaunchesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
